8501e41ab8e27130aa3cb7a2235e0a8532df573c
yudhawan <yudhaashter@gmail.com>
2024-01-18 12:14:56 +0700

    remove unused var

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,8 +1,6 @@
 import style from './ModalWindow.module.scss'
 import { ModalWindowProps } from '../../types'
 import React from 'react'
-import { useAppDispatch } from '../../redux/appHooks'
-import { setToEdit } from '../../redux/productSlice'
 function ModalWindow({children,isOpen,modalId,handleId,handleOpen}:ModalWindowProps) {
   function handleBlur(e:React.MouseEvent<HTMLDivElement>) {
     const target = e.target as HTMLElement
--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice} from '@reduxjs/toolkit'
-import axios from 'axios'
 import { UserType,StatusType } from '../types'
 
 export type typeState = UserType & StatusType
